20f0696e158594c22dabb5089c46a5310720bcb3
shubhamcs <shubham.bagrecha@go-mmt.com>
2023-05-18 20:14:04 +0530

    add interceptor path pattern

--- src/main/java/com/spring/demo/controller/TestController.java
+++ src/main/java/com/spring/demo/controller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path = "/")
+@RequestMapping(path = "/api")
 public class TestController {
   @Autowired private TestRepository testRepository;
   @Autowired private CustomTestRepository customTestRepository;
